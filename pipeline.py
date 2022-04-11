8be81a10e31d61dafafc5c4bbd1219c70c685a03
cayod <cayo.emanuel@gmail.com>
2022-04-11 19:53:03 -0300

    style: mudança de nomeclatura

--- pipeline.py
+++ pipeline.py
@@ -9,7 +9,7 @@ import pandas as pd
 import sqlite3
 
 # Links para download
-link_cnpj = [
+linkCnpj = [
     (
         'http://200.152.38.155/CNPJ/K3241.K03200Y0.D20212.SOCIOCSV.zip'
     ),
@@ -27,14 +27,14 @@ link_cnpj = [
 ]
 
 # Nome das colunas do arquivo empresa.csv
-columns_empresa = [
+columnsEmpresa = [
     'CNPJ BÁSICO', 'RAZÃO SOCIAL/NOME EMPRESARIAL', 'NATUREZA JURÍDICA',
     'QUALIFICAÇÃO DO RESPONSÁVEL', 'CAPITAL SOCIAL DA EMPRESA',
     'PORTE DA EMPRESA', 'ENTE FEDERATIVO RESPONSÁVEL'
 ]
 
 # Nome das colunas do arquivo estabelecimento.csv
-columns_estabelecimento = [
+columnsEstabelecimento = [
     'CNPJ BÁSICO', 'CNPJ ORDEM', 'CNPJ DV', 'IDENTIFICADOR MATRIZ/FILIAL',
     'NOME FANTASIA', 'SITUAÇÃO CADASTRAL', 'DATA SITUAÇÃO CADASTRAL',
     'MOTIVO SITUAÇÃO CADASTRAL', 'NOME DA CIDADE NO EXTERIOR', 'PAIS',
@@ -46,7 +46,7 @@ columns_estabelecimento = [
 ]
 
 # Nome das colunas do arquivo socio.csv
-columns_socio = [
+columnsSocio = [
     'CNPJ BÁSICO', 'IDENTIFICADOR DE SÓCIO',
     'NOME DO SÓCIO (NO CASO PF) OU RAZÃO SOCIAL (NO CASO PJ)',
     'CNPJ/CPF DO SÓCIO', 'QUALIFICAÇÃO DO SÓCIO', 'DATA DE ENTRADA SOCIEDADE',
@@ -55,38 +55,38 @@ columns_socio = [
 ]
 
 # Nomes para os zip
-zip_name = [
+zipName = [
     "1-empresa.zip", "2-estabelecimento.zip", "3-socio.zip", "4-colunas.pdf"]
 
 # Nomes para os arquivos
-file_name = [
+fileName = [
     "1-empresa.csv", "2-estabelecimento.csv", "3-socio.csv", "4-colunas.csv"]
 
 # Nome da database
-database_name = 'cnpj.db'
+databaseName = 'cnpj.db'
 
 # Confirma se os dados estão baixados no repostitório
-file_downloaded_confirm = os.path.exists(
-    zip_name[0]) and os.path.exists(
-        zip_name[1]) and os.path.exists(
-            zip_name[2]) and os.path.exists(zip_name[3])
+fileDownloadedConfirm = os.path.exists(
+    zipName[0]) and os.path.exists(
+        zipName[1]) and os.path.exists(
+            zipName[2]) and os.path.exists(zipName[3])
 
 # Confirma se os dados estão descompactados
-file_unziped_confirm = os.path.exists(
-    file_name[0]) and os.path.exists(
-        file_name[1]) and os.path.exists(file_name[2])
+fileUnzipedConfirm = os.path.exists(
+    fileName[0]) and os.path.exists(
+        fileName[1]) and os.path.exists(fileName[2])
 
 # Download dos arquivos
 
 
-def download_files():
+def downloadFiles():
     # loop para fazer o download de todos os arquivos
     count = 0
-    for url in link_cnpj:
+    for url in linkCnpj:
         print('Downloading...')
         file = wget.download(url)
-        file_path = os.path.basename(file)
-        os.rename(file_path, zip_name[count])
+        filePath = os.path.basename(file)
+        os.rename(filePath, zipName[count])
         count += 1
         print('{} downloaded file'.format(count))
 
@@ -94,57 +94,57 @@ def download_files():
 
 
 def unzip():
-    end_count = 3
-    start_count = 0
-    for start_count in range(end_count):
-        with zipfile.ZipFile(zip_name[start_count], 'r') as unzip:
+    endCount = 3
+    startCount = 0
+    for startCount in range(endCount):
+        with zipfile.ZipFile(zipName[startCount], 'r') as unzip:
             unzip.extractall()
-            change_name = unzip.infolist()[0].filename
-            os.rename(change_name, file_name[start_count])
-            print('{} unziped file'.format(start_count))
+            changeName = unzip.infolist()[0].filename
+            os.rename(changeName, fileName[startCount])
+            print('{} unziped file'.format(startCount))
 
 # Gera a database e insere as tables
 
 
-def create_database():
-    database = sqlite3.connect('database_name')
-    empresa_dataframe = pd.read_csv(
-        file_name[0], encoding="ISO8859-1", sep=',')
-    empresa_dataframe.to_sql(name='empresa', con=database)
-    estabelecimento_dataframe = pd.read_csv(
-        file_name[1], encoding="ISO8859-1", sep=',')
-    estabelecimento_dataframe.to_sql(name='estabelecimento', con=database)
-    socios_dataframe = pd.read_csv(
-        file_name[2], encoding="ISO8859-1", sep=',')
-    socios_dataframe.to_sql(name='socios', con=database)
+def createDatabase():
+    database = sqlite3.connect('databaseName')
+    empresaDataframe = pd.read_csv(
+        fileName[0], encoding="ISO8859-1", sep=',')
+    empresaDataframe.to_sql(name='empresa', con=database)
+    estabelecimentoDataframe = pd.read_csv(
+        fileName[1], encoding="ISO8859-1", sep=',')
+    estabelecimentoDataframe.to_sql(name='estabelecimento', con=database)
+    sociosDataframe = pd.read_csv(
+        fileName[2], encoding="ISO8859-1", sep=',')
+    sociosDataframe.to_sql(name='socios', con=database)
 
 # Pega os dados das colunas para inserir nos csv
 
 
 def dataframe_set_columns():
-    empresa_dataframe = pd.read_csv(
-        file_name[0], encoding="ISO8859-1", names=columns_empresa, sep=';')
-    empresa_dataframe.to_csv(file_name[0])
-    print('Columns set at {}'.format(file_name[0]))
-    estabelecimento_dataframe = pd.read_csv(
-        file_name[1], encoding="ISO8859-1", names=columns_estabelecimento,
+    empresaDataframe = pd.read_csv(
+        fileName[0], encoding="ISO8859-1", names=columnsEmpresa, sep=';')
+    empresaDataframe.to_csv(fileName[0])
+    print('Columns set at {}'.format(fileName[0]))
+    estabelecimentoDataframe = pd.read_csv(
+        fileName[1], encoding="ISO8859-1", names=columnsEstabelecimento,
         sep=';')
-    estabelecimento_dataframe.to_csv(file_name[1])
-    print('Columns set at {}'.format(file_name[1]))
-    socios_dataframe = pd.read_csv(
-        file_name[2], encoding="ISO8859-1", names=columns_socio, sep=';')
-    socios_dataframe.to_csv(file_name[2])
-    print('Columns set at {}'.format(file_name[2]))
-    create_database()
+    estabelecimentoDataframe.to_csv(fileName[1])
+    print('Columns set at {}'.format(fileName[1]))
+    sociosDataframe = pd.read_csv(
+        fileName[2], encoding="ISO8859-1", names=columnsSocio, sep=';')
+    sociosDataframe.to_csv(fileName[2])
+    print('Columns set at {}'.format(fileName[2]))
+    createDatabase()
 
 
 # Fluxo de baixar o arquivo, descompactar, tratar e disponibilizar
-count_flow = 0
-while count_flow == 0:
-    if not file_downloaded_confirm:
-        download_files()
-    elif not file_unziped_confirm:
+countFlow = 0
+while countFlow == 0:
+    if not fileDownloadedConfirm:
+        downloadFiles()
+    elif not fileUnzipedConfirm:
         unzip()
     else:
         dataframe_set_columns()
-        count_flow = 1
+        countFlow = 1
